8b91e6e9eb88b4575310b830dbf97482f59efc8e
Diogogrosario <diogogrosario1008@gmail.com>
2023-03-19 22:15:26 +0000

    New messages for begin, move and end

--- Assets/TouchDetection.cs
+++ Assets/TouchDetection.cs
@@ -7,15 +7,23 @@ public class TouchDetection : MonoBehaviour
         // Check if the screen is being touched
         if (Input.touchCount > 0)
         {
+            UDPSender udpSender = GameObject.Find("UDPSender").GetComponent<UDPSender>();
             // Loop through all touch events
             for (int i = 0; i < Input.touchCount; i++)
             {
                 Touch touch = Input.GetTouch(i);
-
                 if (touch.phase == TouchPhase.Began)
                 {
-                    UDPSender udpSender = GameObject.Find("UDPSender").GetComponent<UDPSender>();
-                    udpSender.Send("Touch:" + touch.position);
+                    
+                    udpSender.Send("ID: " + touch.fingerId + ";Begin-" + touch.position);
+                }
+                else if (touch.phase == TouchPhase.Moved)
+                {
+                    udpSender.Send("ID: " + touch.fingerId + ";Moved-" + touch.position);
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    udpSender.Send("ID: " + touch.fingerId + ";End");
                 }
             }
         }
